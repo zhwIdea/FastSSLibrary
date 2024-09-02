fa2ae279bf7784b125057c6a5a8bd673de255d6e
zhwIdea <zhw_vn@sina.cn>
2024-09-02 00:11:21 -0700

    pod

--- Tun2socks.podspec.swift
+++ Tun2socks.podspec.swift
@@ -5,10 +5,10 @@ Pod::Spec.new do |s|
   s.description     = <<-DESC
 A more detailed description of Tun2socks.
   DESC
-  s.homepage        = 'https://github.com/yourusername/Tun2socks'
+  s.homepage        = 'https://github.com/zhwIdea/FastSSLibrary'
   s.license         = { :type => 'MIT', :file => 'LICENSE' }
 s.author          = { 'zhwIdea' => 'zhw_vn@sina.cn' }
-  s.source          = { :git => 'https://github.com/zhwIdea/FastSSLibrary', :tag => s.version.to_s }
+  s.source          = { :git => 'https://github.com/zhwIdea/FastSSLibrary', :branch => 'main' }
   s.ios.deployment_target = '13.0'
   s.vendored_frameworks = 'Frameworks/Tun2socks.xcframework'
   s.source_files    = 'Frameworks/Tun2socks.xcframework/ios-arm64/**/*.swift' # Adjust as needed
